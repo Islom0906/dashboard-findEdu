0fafca722552246782f2838333036e958a2e00cf
Islom <iabdugofurov99@gmail.com>
2023-02-11 19:19:29 +0500

    crud function

---
+++ src/service/api.js
@@ -0,0 +1,32 @@
+import axios from './axios'
+
+const apiService={
+  // malutmotlarni get qilish uchun url beriladi  
+  // url / boshlansin yani /edu shunga oxshagan
+  async getData(url){
+    const {data} =await axios.get(url)
+    return data
+  },
+   // faqat bitta malumot  get qilish uchun url va idsi beriladi 
+  // url / boshlansin yani /edu shunga oxshagan
+  async getDataByID(url,id){
+    const {data} =await axios.get(`${url}/${id}`)
+    return data
+  },
+   // malutmotni post qilish uchun url va yaratilgan malumot beriladi
+  // url / boshlansin yani /edu shunga oxshagan
+  async postData(url,formData){
+    await axios.post(url,formData)
+  },
+   // malutmotni edit qilish uchun url,id va yaratilgan malumot beriladi
+  // url / boshlansin yani /edu shunga oxshagan
+  async EditData(url,id,formData){
+    await axios.patch(`${url}/${id}`,formData)
+    
+  },
+   // malutmotni delete qilish uchun url va id  beriladi
+  // url / boshlansin yani /edu shunga oxshagan
+  async deleteData(url,id){
+   await axios.delete(`${url}/${id}`)
+  }
+}
