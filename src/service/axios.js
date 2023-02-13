9f26cc236590930d9d48b9fcdfcf11d1276620a7
Karimov Kamoliddin <karimovkamaladdin@gmail.com>
2023-02-13 18:53:50 +0500

    baseUrl jwt to token

--- src/service/axios.js
+++ src/service/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 axios.defaults.baseURL = 'http://18.216.178.179/api/v1'
 
 axios.interceptors.request.use((config) => {
-  const jwt = localStorage.getItem('jwt')
+  const jwt = localStorage.getItem('token')
   const authorization = jwt ? `Bearer ${jwt}` : ''
   config.headers.authorization=authorization
   return config
