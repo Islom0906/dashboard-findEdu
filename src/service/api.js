cdfaacd1ebcaeb5b8d325b0947be3020de10301e
erkiinov <erkinovbavrom12@gmail.com>
2023-02-14 17:07:36 +0500

    kkmas

--- src/service/api.js
+++ src/service/api.js
@@ -1,38 +1,37 @@
-import axios from './axios'
+import axios from './axios';
 
-const apiService={
+const apiService = {
   // user log qilishi uchun url va user malumotlari jonatiladi
-  async userLogin(url,user){
-    const {data}=axios.post(url,user)
-    return data
+  async userLogin(url, user) {
+    const {data} = axios.post(url, user);
+    return data;
   },
-  // malutmotlarni get qilish uchun url beriladi  
+  // malutmotlarni get qilish uchun url beriladi
   // url / boshlansin yani /edu shunga oxshagan
-  async getData(url){
-    const {data} =await axios.get(url)
-    return data
+  async getData(url) {
+    const {data} = await axios.get(url);
+    return data;
   },
-   // faqat bitta malumot  get qilish uchun url va idsi beriladi 
+  // faqat bitta malumot  get qilish uchun url va idsi beriladi
   // url / boshlansin yani /edu shunga oxshagan
-  async getDataByID(url,id){
-    const {data} =await axios.get(`${url}/${id}`)
-    return data
+  async getDataByID(url, id) {
+    const {data} = await axios.get(`${url}/${id}`);
+    return data;
   },
-   // malutmotni post qilish uchun url va yaratilgan malumot beriladi
+  // malutmotni post qilish uchun url va yaratilgan malumot beriladi
   // url / boshlansin yani /edu shunga oxshagan
-  async postData(url,formData){
-    await axios.post(url,formData)
+  async postData(url, formData) {
+    await axios.post(url, formData);
   },
-   // malutmotni edit qilish uchun url,id va yaratilgan malumot beriladi
+  // malutmotni edit qilish uchun url,id va yaratilgan malumot beriladi
   // url / boshlansin yani /edu shunga oxshagan
-  async EditData(url,id,formData){
-    await axios.patch(`${url}/${id}`,formData)
-    
+  async EditData(url, id, formData) {
+    await axios.patch(`${url}/${id}`, formData);
   },
-   // malutmotni delete qilish uchun url va id  beriladi
+  // malutmotni delete qilish uchun url va id  beriladi
   // url / boshlansin yani /edu shunga oxshagan
-  async deleteData(url,id){
-   await axios.delete(`${url}/${id}`)
-  }
-}
-export default apiService
+  async deleteData(url, id) {
+    await axios.delete(`${url}/${id}`);
+  },
+};
+export default apiService;
