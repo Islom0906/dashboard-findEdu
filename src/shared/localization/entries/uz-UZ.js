5eca12edd8fd24afd23bcbb8ab47d9f29c80bf18
Karimov Kamoliddin <karimovkamaladdin@gmail.com>
2023-02-16 21:44:26 +0500

    uzb lang

---
+++ src/shared/localization/entries/uz-UZ.js
@@ -0,0 +1,11 @@
+import uzMessages from '../locales/uz_UZ.json';
+import uzUZ from 'antd/lib/locale/ru_RU';
+
+const UzLang = {
+  messages: {
+    ...uzMessages,
+  },
+  antLocale: uzUZ,
+  locale: 'uz-UZ',
+};
+export default UzLang;
--- src/shared/localization/index.js
+++ src/shared/localization/index.js
@@ -1,9 +1,11 @@
 import enLang from './entries/en-US';
 import ruLang from './entries/ru-RU';
+import uzLang from './entries/uz-UZ';
 
 const AppLocale = {
   en: enLang,
   ru: ruLang,
+  uz: uzLang,
 };
 
 export default AppLocale;
