978e91b820507ea6cf5fc25398679cd411fdb06b
erkiinov <erkinovbavrom12@gmail.com>
2023-02-18 04:58:30 +0500

    nth

--- src/pages/auth/Signin/index.js
+++ src/pages/auth/Signin/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.style.less';
 import AuthWrapper from '../AuthWrapper';
 import AppPageMetadata from '../../../@crema/core/AppPageMetadata';
-import SignInFirebase from './SigninFirebase'; 
+import SignInFirebase from './SigninFirebase';
 ////commit
 const Signin = () => {
   return (
