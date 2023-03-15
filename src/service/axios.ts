ca84bf36da326840ff88ebcc5b426d3aebf89602
Fazliddin <99480480+FazliddinMirzaqosimov@users.noreply.github.com>
2023-03-15 19:59:25 +0500

    api changed

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -1,6 +1,6 @@
 import axios, {AxiosRequestConfig} from 'axios';
 
-axios.defaults.baseURL = 'http://18.216.178.179/api/v1';
+axios.defaults.baseURL = 'http://18.221.130.228/api/';
 
 axios.interceptors.request.use((config: AxiosRequestConfig) => {
   const jwt: string | null = localStorage.getItem('token');
