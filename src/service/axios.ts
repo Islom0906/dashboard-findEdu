733f84edc85f4f6a630bb76f76264ebdc47ec5c1
Abdujabbarof <abdujabborov6172@gmail.com>
2023-04-20 18:28:21 +0500

    nimadir

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -1,6 +1,6 @@
 import axios, {AxiosRequestConfig} from 'axios';
 
-axios.defaults.baseURL = 'http://18.221.130.228/api/';
+axios.defaults.baseURL = 'http://3.138.61.64/';
 
 axios.interceptors.request.use((config: AxiosRequestConfig) => {
   const jwt: string | null = localStorage.getItem('token');
